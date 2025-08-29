41a85bd34e4de778ce03c95782b9e054dfba7f22
Prashant Adhikari <prashantadhikareeey@gmail.com>
2025-08-29 08:12:44 +0545

    cors removed

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 require("dotenv").config();
 const express = require("express");
-const cors = require("cors");
 const connectDB = require("./services/connectDataBase");
 const CronService = require("./services/cronService");
 
@@ -16,7 +15,6 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
 app.use(express.json());
 
 // Health check
